6aebccabdab29a5a9b6907baba98de025c570460
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2024-01-19 13:37:21 +0800

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Sifiso Dhlamini on 2024/1/19.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
